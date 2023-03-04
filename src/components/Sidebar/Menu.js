99ddd2f138953e1d6984e552cf61ab531d2abe8f
miray çetin <203301029@ogr.selcuk.edu.tr>
2023-03-04 14:00:06 +0300

    addicons

--- src/components/Sidebar/Menu.js
+++ src/components/Sidebar/Menu.js
@@ -1,19 +1,30 @@
+import { Icon } from "Icons";
+
 function Menu() {
     return (
         <nav className="px-2">
             <ul className="flex flex-col">
                 <li>
-                    <a href="#" className="h-10 flex items-center text-sm">
+                    <a href="#" className="h-10 flex gap-x-4 items-center text-sm font-semibold text-white rounded hover:text-white px-4 bg-active">
+                        <span>
+                            <Icon name="home" />
+                        </span>
                         Anasayfa
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="h-10 flex items-center text-sm">
+                    <a href="#" className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4">
+                        <span>
+                            <Icon name="search" />
+                        </span>
                         Ara
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="h-10 flex items-center text-sm">
+                    <a href="#" className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4">
+                         <span>
+                            <Icon name="collection" />
+                        </span>
                         Kitaplığın
                     </a>
                 </li>
