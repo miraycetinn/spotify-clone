c1d34e4ae61d469a5ebf2bfe4f694123b470dc9d
Miray Çetin <203301029@ogr.selcuk.edu.tr>
2024-03-17 02:13:34 +0300

    useNavigate for arrows headlessUI installed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,27 @@
 import React from "react";
 import UserWidget from "./UserWidget";
+import { useNavigate } from "react-router-dom"
 function Navbar() {
+
+    const navigate = useNavigate()
+
+    const handleBackClick = () => {
+        navigate(-1); // Go back one history step
+    };
+
+    const handleForwardClick = () => {
+        navigate(1); // Go forward one history step
+    };
     return (
         <nav class="bg-black w-full h-16">
             <div className="flex justify-between">
                 <div className="flex items-center pl-8 gap-x-4">
-                    <button className="bg-gray-900 w-8 h-8 opacity-90 rounded-full">
+                    <button onClick={handleBackClick} className="bg-gray-900 w-8 h-8 opacity-90 rounded-full">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="m-auto w-6 h-6">
                             <path d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z"/>
                         </svg>
                     </button>
-                    <button className="bg-gray-900 w-8 h-8 opacity-90 rounded-full">
+                    <button onClick={handleForwardClick} className="bg-gray-900 w-8 h-8 opacity-90 rounded-full">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className=" m-auto w-6 h-6">
                             <path d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"/>
                         </svg>
