67198ee0fe9952f3a0922d17706b2616e557453d
Miray Çetin <203301029@ogr.selcuk.edu.tr>
2024-03-20 23:22:58 +0300

    update

--- src/components/UserWidget.js
+++ src/components/UserWidget.js
@@ -6,7 +6,7 @@ function UserWidget({ name, imageUrl }) {
     return (
         <Menu as="Nav" className={"relative"}> {/* dropdownun yana açılması engellendi */}
                     <Menu.Button
-                        className=" bg-gray-900 opacity-90 hover:bg-gray-700 rounded-full flex items-center">
+                        className=" bg-gray-800 opacity-90 hover:bg-gray-700 rounded-full flex items-center">
                         <img className="rounded-full w-8 h-8 object-cover mr-2" src={imageUrl} alt="user profile"/>
                         <span className=" pr-3 text-sm font-semibold">{name}</span>
                         <svg role="img" height="16" width="16" aria-hidden="true" fill="#FFFFFF"
