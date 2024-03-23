b2f7f370aad38f4dfc7cda23b949d52e3037244e
Miray Çetin <203301029@ogr.selcuk.edu.tr>
2024-03-23 23:10:50 +0300

    fix

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -4,7 +4,7 @@ const AlbumCard = ({photo, name, date, long}) => {
 
 
     return (
-        <div className="flex justify-between flex-col hover:bg-white/[.10] bg-white/[.03] hover:cursor-pointer"
+        <div className="flex justify-between flex-col hover:bg-white/[.10] bg-black/[.03] hover:cursor-pointer"
              style={{width: "181px", height: "259px", padding: "16px"}}>
             <div>{photo}</div>
             <div><strong>{name}</strong></div>
